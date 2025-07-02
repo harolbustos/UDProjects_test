71af62c11e6b7737b5b7933e819227b5d360d2b5
harol <harolbustos7@gmail.com>
2025-07-02 11:43:03 -0400

    fix: URL front

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -16,15 +16,15 @@ router = APIRouter()
 IS_PROD = os.getenv("RAILWAY_ENVIRONMENT") == "production"
 
 FRONTEND_URL = (
-    "http://localhost:5173"
-    # os.getenv("FRONTEND_URL_VERCEL")
-    # if IS_PROD
-    # else "http://localhost:5173"
+    # "http://localhost:5173"
+    os.getenv("FRONTEND_URL_VERCEL")
+    if IS_PROD
+    else "http://localhost:5173"
 )
 
 redirect_uri = (
-    os.getenv("BACKEND_URL_RAILWAY")
     # "http://localhost:8000"
+    os.getenv("BACKEND_URL_RAILWAY")
     if IS_PROD
     else "http://localhost:8000"
 )
